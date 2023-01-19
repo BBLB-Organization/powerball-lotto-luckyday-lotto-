1fb01793c80db32e421b4b48b9d042f805cbaa6c
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-19 16:55:40 -0600

    Can now randomly generate random lottery numbers and user can now see the numbers they selected

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -11,10 +11,12 @@ export class HomepageComponent implements OnInit {
   numbers = Array.from({ length: 45 }, (_, i) => i + 1);
   numberRows = Array.from({ length: 7 }, (_, i) => this.numbers.slice(i * 7, (i * 7) + 7));
   pickedNumbers: number[] = [];
+  randomLotteryNumbersSelected: number[] = [];
+  randomLotteryNumberFinished: boolean = false;
 
   pickNumber(number: number) {
     number = number + 1;  //Fixes the numbers since it's off by one
-    
+
     //Checks if picked numbers hasn't exceeded 5 and number hasn't been picked twice
     if (this.pickedNumbers.length < this.luckyDayLottoNumPicksLength && !this.pickedNumbers.includes(number)) {
       this.pickedNumbers.push(number);
@@ -23,17 +25,40 @@ export class HomepageComponent implements OnInit {
     else if (this.pickedNumbers.includes(number)) {
       this.pickedNumbers = this.pickedNumbers.filter(a => a != number);
     }
+    this.pickedNumbers = this.pickedNumbers.sort((n1,n2) => n1 - n2);
   }
 
   clearNumbers() {
     this.pickedNumbers = [];
   }
 
-
+  getRandom() {
+    const minNumber: number = 1;
+    const maxNumber: number = 45;
+    const floatRandom = Math.random()
+    const difference = maxNumber - minNumber;
+    // random between 0 and the difference
+    const random = Math.round(difference * floatRandom)
+    const randomWithinRange = random + minNumber;
+    console.log(randomWithinRange);
+    if (this.randomLotteryNumbersSelected.includes(randomWithinRange)) {
+      return;
+    }
+    else {
+      this.randomLotteryNumbersSelected.push(randomWithinRange);
+      if (this.randomLotteryNumbersSelected.length == 5) {
+        this.randomLotteryNumberFinished = true;
+      }
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    do {
+      this.getRandom();
+    } while (!this.randomLotteryNumberFinished);
+    console.log(this.randomLotteryNumbersSelected);
   }
 
 }
