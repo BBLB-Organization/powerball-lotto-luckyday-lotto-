508eca75e7492c8c96698bb6f72c95768ce461b4
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-19 09:55:26 -0600

    User can now select and deselect the number they want to play

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
 
   luckyDayLottoNumPicksLength: number = 5;
-  numbers = Array.from({length: 49}, (_, i) => i + 1);
-  numberRows = Array.from({length: 7}, (_, i) => this.numbers.slice(i*7, (i*7)+7));
+  numbers = Array.from({ length: 45 }, (_, i) => i + 1);
+  numberRows = Array.from({ length: 7 }, (_, i) => this.numbers.slice(i * 7, (i * 7) + 7));
   pickedNumbers: number[] = [];
 
   pickNumber(number: number) {
-    if (this.pickedNumbers.length < this.luckyDayLottoNumPicksLength) {
-        this.pickedNumbers.push(number + 1);
+    number = number + 1;  //Fixes the numbers since it's off by one
+    
+    //Checks if picked numbers hasn't exceeded 5 and number hasn't been picked twice
+    if (this.pickedNumbers.length < this.luckyDayLottoNumPicksLength && !this.pickedNumbers.includes(number)) {
+      this.pickedNumbers.push(number);
+    }
+    //Checks if number has been picked twice
+    else if (this.pickedNumbers.includes(number)) {
+      this.pickedNumbers = this.pickedNumbers.filter(a => a != number);
     }
   }
 
