59f61bbdbcb5b9b92df5366d3392b61406544445
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-21 15:40:22 -0600

    Decided best practice is to put all pages into pages folder, added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OddsTableComponent } from './components/odds-table/odds-table.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     NavbarComponent,
-    OddsTableComponent
+    OddsTableComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
