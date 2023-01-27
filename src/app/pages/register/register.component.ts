989dffdde400aee56e11b66cfac3e7397ef70e44
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-26 21:29:00 -0600

    Users can now register to my website and will be added to the database of users

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Users } from 'src/app/models/Users/users.model';
 import { UserService } from 'src/app/services/UserService/user.service';
 
@@ -10,7 +11,7 @@ import { UserService } from 'src/app/services/UserService/user.service';
 })
 export class RegisterComponent implements OnInit {
 
-  newUser: Users | undefined;
+  newUser?: Users;
   registerForm: FormGroup = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
@@ -27,23 +28,31 @@ export class RegisterComponent implements OnInit {
   get email(): string { return this.registerForm.get('email')?.value; }
   get password(): string { return this.registerForm.get('password')?.value; }
 
-  onSubmit() {
+  prepareSave(): Users {
+    return new Users(
+      null,
+      this.username,
+      this.email,
+      this.password,
+      "",
+      ""
+    )
+  }
+
+  registerUser(): void {
     if (this.registerForm.valid) {
-      let fakeEmail = this.registerForm.get('email')!.value;
+      let registeredUser = this.prepareSave();
+      console.log(registeredUser);
+      this.userService.registerUser(registeredUser).subscribe();
+      this.router.navigate(['/homepage']);
     }
   }
 
-  registerUserTestButton() {
-    console.log("You hit register button!");
-    let user: Users = new Users(null, "", "", "", "", "");
-    user.username = "jmorin-testUserName";
-    user.emailAddress = "jmorintest@email.com";
-    user.password = "testPass"
-    console.log(user);
-    this.userService.registerUser(user).subscribe();
-  }
-
-  constructor(private fb: FormBuilder, private userService: UserService) { }
+  constructor(
+    private fb: FormBuilder, 
+    private userService: UserService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
 
