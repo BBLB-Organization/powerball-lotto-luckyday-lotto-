130d1823c0b238146ec8f9d226a05a92388cd71c
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-31 17:30:08 -0600

    Added styling on user stats, need to build out back end to handle user stat data

--- src/app/pages/user-stats/user-stats.component.ts
+++ src/app/pages/user-stats/user-stats.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-stats',
   templateUrl: './user-stats.component.html',
   styleUrls: ['./user-stats.component.css']
 })
-export class UserStatsComponent {
+export class UserStatsComponent implements OnInit {
+
+  listOfCardTitles: string [] = ["TOTAL GAMES PLAYED","TOTAL GAMES WON", "MATCHED 2 BALLS", "MATCHED 3 BALLS", "MATCHED 4 BALLS", "JACKPOT!"];
+  tempNumberList : number [] = [187, 35, 30, 5, 0, 0];
+
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+  }
+  
 
 }
